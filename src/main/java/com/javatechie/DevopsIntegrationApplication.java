89db35b2a56b06ef1b32e8fa10e75e3aad7a191e
demo <demo@example.com>
2022-08-27 12:00:53 +0530

    new-commit

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to javatechie vishal";
 	}
 
 	public static void main(String[] args) {
